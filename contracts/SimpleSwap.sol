6da8a19da77eeda264cff5eaae4e0eeaac799477
Tommy <tommy50101@gmail.com>
2022-11-24 03:37:15 +0800

    chore: Optimized variables and functions naming

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -15,8 +15,8 @@ contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
     uint256 public reserveB;
 
     constructor(address _tokenA, address _tokenB) ERC20("TestErc20Token", "TET") {
-        require(_checkContract(_tokenA), "SimpleSwap: TOKENA_IS_NOT_CONTRACT");
-        require(_checkContract(_tokenB), "SimpleSwap: TOKENB_IS_NOT_CONTRACT");
+        require(_isContract(_tokenA), "SimpleSwap: TOKENA_IS_NOT_CONTRACT");
+        require(_isContract(_tokenB), "SimpleSwap: TOKENB_IS_NOT_CONTRACT");
         require(_tokenA != _tokenB, "SimpleSwap: TOKENA_TOKENB_IDENTICAL_ADDRESS");
         tokenA = ERC20(_tokenA);
         tokenB = ERC20(_tokenB);
@@ -25,87 +25,87 @@ contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
     /**
         Check whether a addres is a contract
      */
-    function _checkContract(address addr) private view returns (bool) {
+    function _isContract(address _addr) private view returns (bool) {
         // kaccak result of empty data
-        bytes32 emptyAccountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        bytes32 codehash;
+        bytes32 _emptyAccountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        bytes32 _codehash;
         assembly {
-            codehash := extcodehash(addr)
+            _codehash := extcodehash(_addr)
         }
-        return (codehash != 0x0 && codehash != emptyAccountHash);
+        return (_codehash != 0x0 && _codehash != _emptyAccountHash);
     }
 
     /// @notice Swap tokenIn for tokenOut with amountIn
-    /// @param tokenIn The address of the token to swap from
-    /// @param tokenOut The address of the token to swap to
-    /// @param amountIn The amount of tokenIn to swap
-    /// @return amountOut The amount of tokenOut received
+    /// @param _tokenIn The address of the token to swap from
+    /// @param _tokenOut The address of the token to swap to
+    /// @param _amountIn The amount of tokenIn to swap
+    /// @return _amountOut The amount of tokenOut received
     function swap(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn
     ) external nonReentrant returns (uint256) {
         // forces error, when tokenIn is not tokenA or tokenB
-        require((tokenIn == address(tokenA) || tokenIn == address(tokenB)), "SimpleSwap: INVALID_TOKEN_IN");
+        require(_tokenIn == address(tokenA) || _tokenIn == address(tokenB), "SimpleSwap: INVALID_TOKEN_IN");
         // forces error, when tokenOut is not tokenA or tokenB
-        require((tokenOut == address(tokenA) || tokenOut == address(tokenB)), "SimpleSwap: INVALID_TOKEN_OUT");
+        require(_tokenOut == address(tokenA) || _tokenOut == address(tokenB), "SimpleSwap: INVALID_TOKEN_OUT");
         // forces error, when tokenIn is the same as tokenOut
-        require(tokenIn != tokenOut, "SimpleSwap: IDENTICAL_ADDRESS");
+        require(_tokenIn != _tokenOut, "SimpleSwap: IDENTICAL_ADDRESS");
         // forces error, when amountIn is zero
-        require(amountIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+        require(_amountIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
 
-        address sender = _msgSender();
-        uint256 originTokenIn = ERC20(tokenIn).balanceOf(address(this));
-        uint256 originTokenOut = ERC20(tokenOut).balanceOf(address(this));
+        address _sender = _msgSender();
+        uint256 _originTokenIn = ERC20(_tokenIn).balanceOf(address(this));
+        uint256 _originTokenOut = ERC20(_tokenOut).balanceOf(address(this));
 
         // 算法1 (減法後面那串的結果的小數點會被捨去，相剪出來的值會大於實際可以轉出的值，所以這種算法會有錯):
-        // uint256 amountOut = originTokenOut - (originTokenOut * originTokenIn / (originTokenIn + amountIn)) ;
+        // uint256 _amountOut = _originTokenOut - (_originTokenOut * _originTokenIn / (_originTokenIn + _amountIn)) ;
 
         // 算法2:
         // x * y = x' * y' = k
-        // originTokenOut * originTokenIn = (originTokenIn + amountIn) * (originTokenOut - amountOut)
-        // originTokenOut * originTokenIn = originTokenIn * originTokenOut - originTokenIn * amountOut + amountIn * originTokenOut - amountIn * amountOut
-        // originTokenIn * amountOut + amountIn * amountOut = originTokenIn * originTokenOut + amountIn * originTokenOut - originTokenOut * originTokenIn
-        // amountOut * (originTokenIn + amountIn) = originTokenIn * originTokenOut + amountIn * originTokenOut - originTokenOut * originTokenIn
-        // amountOut = originTokenIn * originTokenOut + amountIn * originTokenOut - originTokenOut * originTokenIn / (originTokenIn + amountIn)
-        // amountOut = originTokenOut * (originTokenIn + amountIn - originTokenIn) / (originTokenIn + amountIn)
-        // amountOut = originTokenOut * amountIn / (originTokenIn + amountIn)
-        uint256 amountOut = (originTokenOut * amountIn) / (originTokenIn + amountIn);
+        // => _originTokenOut * _originTokenIn = (_originTokenIn + _amountIn) * (_originTokenOut - _amountOut)
+        // => _originTokenOut * _originTokenIn = _originTokenIn * _originTokenOut - _originTokenIn * _amountOut + _amountIn * _originTokenOut - _amountIn * _amountOut
+        // => _originTokenIn * _amountOut + _amountIn * _amountOut = _originTokenIn * _originTokenOut + _amountIn * _originTokenOut - _originTokenOut * _originTokenIn
+        // => _amountOut * (_originTokenIn + _amountIn) = _originTokenIn * _originTokenOut + _amountIn * _originTokenOut - _originTokenOut * _originTokenIn
+        // => _amountOut = originTokenIn * _originTokenOut + _amountIn * _originTokenOut - _originTokenOut * _originTokenIn / (_originTokenIn + _amountIn)
+        // => _amountOut = _originTokenOut * (_originTokenIn + _amountIn - _originTokenIn) / (_originTokenIn + _amountIn)
+        // => _amountOut = _originTokenOut * _amountIn / (_originTokenIn + _amountIn)
+        uint256 _amountOut = (_originTokenOut * _amountIn) / (_originTokenIn + _amountIn);
 
         // forces error, when amountOut is zero
-        require(amountOut > 0, "SimpleSwap: INSUFFICIENT_OUTPUT_AMOUNT");
+        require(_amountOut > 0, "SimpleSwap: INSUFFICIENT_OUTPUT_AMOUNT");
 
         // Make sure k is the same after swap (有時會有無窮小數問題，所以這邊改成 >=)
         require(
-            (originTokenIn + amountIn) * (originTokenOut - amountOut) >= originTokenIn * originTokenOut,
+            (_originTokenIn + _amountIn) * (_originTokenOut - _amountOut) >= _originTokenIn * _originTokenOut,
             "SimpleSwap: UNEXPECTED_K"
         );
 
         // Update reserves
-        if (tokenIn == address(tokenA)) {
-            reserveA = tokenA.balanceOf(address(this)) + amountIn;
-            reserveB = tokenB.balanceOf(address(this)) - amountOut;
-        } else if (tokenIn == address(tokenB)) {
-            reserveA = tokenA.balanceOf(address(this)) - amountOut;
-            reserveB = tokenB.balanceOf(address(this)) + amountIn;
+        if (_tokenIn == address(tokenA)) {
+            reserveA = tokenA.balanceOf(address(this)) + _amountIn;
+            reserveB = tokenB.balanceOf(address(this)) - _amountOut;
+        } else if (_tokenIn == address(tokenB)) {
+            reserveA = tokenA.balanceOf(address(this)) - _amountOut;
+            reserveB = tokenB.balanceOf(address(this)) + _amountIn;
         }
 
         // Action swap
-        ERC20(tokenIn).transferFrom(sender, address(this), amountIn);
-        ERC20(tokenOut).transfer(sender, amountOut);
+        ERC20(_tokenIn).transferFrom(_sender, address(this), _amountIn);
+        ERC20(_tokenOut).transfer(_sender, _amountOut);
 
-        emit Swap(sender, tokenIn, tokenOut, amountIn, amountOut);
+        emit Swap(_sender, _tokenIn, _tokenOut, _amountIn, _amountOut);
 
-        return amountOut;
+        return _amountOut;
     }
 
     /// @notice Add liquidity to the pool
-    /// @param amountAIn The amount of tokenA to add
-    /// @param amountBIn The amount of tokenB to add
-    /// @return amountA The actually amount of tokenA added
-    /// @return amountB The actually amount of tokenB added
-    /// @return liquidity The amount of liquidity minted
-    function addLiquidity(uint256 amountAIn, uint256 amountBIn)
+    /// @param _amountAIn The amount of tokenA to add
+    /// @param _amountBIn The amount of tokenB to add
+    /// @return _actualAmountAIn The actually amount of tokenA added
+    /// @return _actualAmountBIn The actually amount of tokenB added
+    /// @return _liquidity The amount of liquidity minted
+    function addLiquidity(uint256 _amountAIn, uint256 _amountBIn)
         external nonReentrant
         returns (
             uint256,
@@ -115,18 +115,20 @@ contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
     {
         // forces error, when tokenA amount is zero
         // forces error, when tokenB amount is zero
-        require(amountAIn > 0 && amountBIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+        require(_amountAIn > 0 && _amountBIn > 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
 
         address _sender = _msgSender();
         uint256 _totalSupply = totalSupply();
         uint256 _liquidity;
-        uint256 _actualAmountAIn = amountAIn;
-        uint256 _actualAmountBIn = amountBIn;
+        uint256 _actualAmountAIn;
+        uint256 _actualAmountBIn;
 
         if (_totalSupply == 0) {
-            _liquidity = Math.sqrt(amountAIn * amountBIn);
+            _liquidity = Math.sqrt(_amountAIn * _amountBIn);
+            _actualAmountAIn = _amountAIn;
+            _actualAmountBIn = _amountBIn;
         } else {
-            _liquidity = Math.min((amountAIn * _totalSupply) / reserveA, (amountBIn * _totalSupply) / reserveB);
+            _liquidity = Math.min((_amountAIn * _totalSupply) / reserveA, (_amountBIn * _totalSupply) / reserveB);
             _actualAmountAIn = (_liquidity * reserveA) / _totalSupply;
             _actualAmountBIn = (_liquidity * reserveB) / _totalSupply;
         }
@@ -139,6 +141,7 @@ contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
         tokenA.transferFrom(_sender, address(this), _actualAmountAIn);
         tokenB.transferFrom(_sender, address(this), _actualAmountBIn);
 
+        // Action mint (Add liquidity)
         _mint(_sender, _liquidity);
 
         emit AddLiquidity(_sender, _actualAmountAIn, _actualAmountBIn, _liquidity);
@@ -147,20 +150,20 @@ contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
     }
 
     /// @notice Remove liquidity from the pool
-    /// @param liquidity The amount of liquidity to remove
-    /// @return amountA The amount of tokenA received
-    /// @return amountB The amount of tokenB received
-    function removeLiquidity(uint256 liquidity) external nonReentrant returns (uint256, uint256) {
+    /// @param _liquidity The amount of liquidity to remove
+    /// @return _amountAOut The amount of tokenA received
+    /// @return _amountBOut The amount of tokenB received
+    function removeLiquidity(uint256 _liquidity) external nonReentrant returns (uint256, uint256) {
         // forces error, when lp token amount is zero
-        require(liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
+        require(_liquidity > 0, "SimpleSwap: INSUFFICIENT_LIQUIDITY_BURNED");
 
         address _sender = _msgSender();
         uint256 _totalSupply = totalSupply();
-        uint256 _amountAOut = (liquidity * reserveA) / _totalSupply;
-        uint256 _amountBOut = (liquidity * reserveB) / _totalSupply;
+        uint256 _amountAOut = (_liquidity * reserveA) / _totalSupply;
+        uint256 _amountBOut = (_liquidity * reserveB) / _totalSupply;
 
-        _transfer(_sender, address(this), liquidity);
-        _burn(address(this), liquidity);
+        _transfer(_sender, address(this), _liquidity);
+        _burn(address(this), _liquidity);
 
         // Update reserves
         reserveA = tokenA.balanceOf(address(this)) - _amountAOut;
@@ -170,7 +173,7 @@ contract SimpleSwap is ISimpleSwap, ERC20, ReentrancyGuard {
         tokenA.transfer(_sender, _amountAOut);
         tokenB.transfer(_sender, _amountBOut);
 
-        emit RemoveLiquidity(_sender, _amountAOut, _amountBOut, liquidity);
+        emit RemoveLiquidity(_sender, _amountAOut, _amountBOut, _liquidity);
 
         return (_amountAOut, _amountBOut);
     }
